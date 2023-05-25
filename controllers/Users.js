67e0d7ab230c9a41b5776ad06eb9557dd120ae48
Tanjiro Senpai <kielsyanderson@gmail.com>
2023-05-25 08:49:50 +0800

    main

--- controllers/Users.js
+++ controllers/Users.js
@@ -4,20 +4,25 @@ module.exports = async (req, res) => {
   const { ip } = req.query;
   const type = req.params.type;
   if (type == "view") {
-    const newView = new User({
-      viewers_ip: ip,
-      refresh: true,
-    });
-    const savedView = newView.save();
+    const old = await User.findOne({ ip });
+    if (!old) {
+      const newView = new User({
+        viewers_ip: ip,
+        refresh: true,
+      });
+      const savedView = newView.save();
 
-    return res.status(201).json({ result: savedView });
+      return res.status(201).json({ result: "Success!" });
+    } else {
+      return res.status(200).json({ message: "Already Addded!" });
+    }
   } else if (type == "refresh") {
     const newRefresh = new User({
       refresh: true,
     });
     const savedRefresh = newRefresh.save();
 
-    return res.status(201).json({ result: savedRefresh });
+    return res.status(201).json({ result: "Success!" });
   } else {
     res.status(500).json({ message: "Wrong Type!", code: 400 });
   }
