b147a4713afd7dc6423a52f414236abbb6740147
Tanjiro Senpai <kielsyanderson@gmail.com>
2023-05-26 16:06:08 +0800

    main

--- controllers/Users.js
+++ controllers/Users.js
@@ -2,8 +2,7 @@ const User = require("../models/Users.js");
 
 module.exports.Update = async (req, res) => {
   const { ip } = req.query;
-  const type = req.params.type;
-  if (type == "view") {
+  try {
     const old = await User.findOne({ viewers_ip: ip });
     if (!old) {
       const newView = new User({
@@ -14,17 +13,15 @@ module.exports.Update = async (req, res) => {
 
       return res.status(201).json({ result: "Success!" });
     } else {
-      return res.status(200).json({ message: "Already Addded!" });
-    }
-  } else if (type == "refresh") {
-    const newRefresh = new User({
-      refresh: true,
-    });
-    newRefresh.save();
+      const newRefresh = new User({
+        refresh: true,
+      });
+      newRefresh.save();
 
-    return res.status(201).json({ result: "Success!" });
-  } else {
-    res.status(500).json({ message: "Wrong Type!", code: 400 });
+      return res.status(201).json({ result: "Success!" });
+    }
+  } catch (error) {
+    res.status(500).json({ error, code: 400 });
   }
 };
 
